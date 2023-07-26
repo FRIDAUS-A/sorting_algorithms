468026403bffa5e5cd2056fc879af34e5e0e0fcb
FRIDAUS-A <ayobamifridaus@gmail.com>
2023-07-26 11:13:25 +0100

    Bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,8 +10,10 @@
 void bubble_sort(int *array, size_t size)
 {
 	int index, outer_index, tmp_1, tmp_2, tmp_size, i;
-	tmp_size = size;
 
+	if (array = NULL || size == 0)
+		return;
+	tmp_size = size;
 	for (outer_index = 0; (outer_index < tmp_size) && (tmp_size >= 2); outer_index++)
 	{
 		for (index = 0; index < tmp_size; index++)
@@ -21,15 +23,7 @@ void bubble_sort(int *array, size_t size)
 				tmp_1 = array[index];
 				array[index] = array[index + 1];
 				array[index + 1] = tmp_1;
-				i = 0;
-				while (array && i < tmp_size)
-				{
-					if (i > 0)
-						printf(", ");
-					printf("%d", array[i]);
-					++i;
-				}
-				printf("\n");
+				print_array(array, size);
 			}
 		}
 	}
