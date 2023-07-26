1c227114fca36b7d6461eb06dad672100ccfffa0
FRIDAUS-A <ayobamifridaus@gmail.com>
2023-07-26 12:04:19 +0100

    Header

--- sort.h
+++ sort.h
@@ -1,6 +1,10 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -8,9 +12,6 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
 */
-
-#include <stddef.h>
-#include <stdlib.h>
 
 typedef struct listint_s
 {
