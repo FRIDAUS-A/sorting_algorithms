8e7e2e136dd61d4399d56d8d3939850e9489c7dd
FRIDAUS-A <ayobamifridaus@gmail.com>
2023-07-27 11:41:17 +0100

    Selection sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+void selection_sort(int *array, size_t size)
+{
+	size_t index, outer_index = 0;
+	int tmp, check_index, check;
+
+	while (outer_index < size - 1)
+	{
+		tmp = array[outer_index];
+		check = 0;
+		for (index = outer_index; index < size; index++)
+		{
+			if (tmp > array[index])
+			{
+				tmp = array[index];
+				check_index = index;
+				check = 1;
+			}
+		}
+		array[check_index] = array[outer_index];
+		array[outer_index] = tmp;
+		if (check == 1)
+			print_array(array, size);
+		outer_index++;
+	}
+}
